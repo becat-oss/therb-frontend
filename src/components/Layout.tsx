1cd6e721418ccb81cbd2e4dec4f82a4815cb3f7e
Shrestha Prasun <shresthaprasun1@gmail.com>
2022-10-29 13:23:48 +0900

    projects refactored

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import { AppProvider } from 'src/AppContext';
-import Header from './Header';
+import React from "react";
+import { AppProvider } from "src/AppContext";
+import Header from "./Header";
 
-interface LayoutProps{
-    children: React.ReactNode;
+interface LayoutProps {
+  children: React.ReactNode;
 }
-export default function Layout(props: LayoutProps):React.ReactElement {
-    const { children }=props;
-    
-    return(
-        <AppProvider>
-            <Header />
-            {children}
-        </AppProvider>
-        
-    )
+export default function Layout(props: LayoutProps): React.ReactElement {
+  const { children } = props;
+
+  return (
+    <AppProvider>
+      <div style={{ 
+        display: "grid", 
+        width: "100%", 
+        height: "97vh",
+        gridTemplateRows: "4em 1fr"
+        }}>
+        <Header />
+        {children}
+      </div>
+    </AppProvider>
+  );
 }
