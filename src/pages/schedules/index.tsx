9ec2c71b8b31cc11e3459eb93a49ccf0be2d6414
katsuya0719 <katsuya0719@gmail.com>
2022-11-26 19:58:32 +0900

    rev

--- src/pages/schedules/index.tsx
+++ src/pages/schedules/index.tsx
@@ -198,5 +198,5 @@ export async function getStaticProps() {
   const scheduleDetails = await getSchedules_API();
   console.log(scheduleDetails);
   // Pass data to the page via props
-  return { props: { scheduleDetails } };
+  return { props: { scheduleDetails }, revalidate: 10 };
 }
